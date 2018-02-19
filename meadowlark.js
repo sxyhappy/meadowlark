f4d965cefa2e3fae40bb5b41189f55ef3781e0c0
shixiaoyu <sxyhappiness@sina.com>
2018-02-19 08:57:02 +0800

    add hood-river and request-group-rate page

--- meadowlark.js
+++ meadowlark.js
@@ -11,12 +11,25 @@ app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'hbs');
 app.set('views', './views');
 
+app.use((req, res, next) => {
+  res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+  next();
+});
+
 app.get('/', (req, res) => {
   res.render('home');
 });
 
 app.get('/about', (req, res) => {
   res.render('about', { fortune: fortune.getFortune() });
+});
+
+app.get('/tours/hood-river', (req, res) => {
+  res.render('tours/hood-river');
+});
+
+app.get('/tours/request-group-rate', (req, res) => {
+  res.render('tours/request-group-rate');
 });
 
 app.use((req, res) => {
